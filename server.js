e9a03747907057fab22489ed7b45931322aa993d
Jussi Vesala <jussi.vesala@reaktor.fi>
2015-11-27 16:31:45 +0200

    add initial mockserver

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Default Content-Type to Application/Json
+app.use(function(req, res, next) { 
+  res.setHeader("Content-Type", "application/json")
+  return next() 
+})
+
+app.post('/tek/api/v1/requestContact/', function(req, res){
+  console.log("WE GOT" + JSON.stringify(req.body))
+  res.send({result: "OK"})
+})
+
+var appPort = process.env.PORT || 3000
+console.log("Mock server listening " + appPort)
+app.listen(appPort)
